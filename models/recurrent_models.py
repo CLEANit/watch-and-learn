65069f042c078abbf76dc8e6084aee107c2406b8
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2020-04-18 20:08:44 -0500

    multiple datasets for validation

--- models/recurrent_models.py
+++ models/recurrent_models.py
@@ -14,13 +14,15 @@ class ProbabilityRNN(pl.LightningModule):
         self.hparams = hparams
         if hparams.lstm:
             self.rnn = nn.LSTM(hparams.input_size, hparams.hidden_size,
-                              num_layers=hparams.num_layers, batch_first=True)
+                               num_layers=hparams.num_layers, batch_first=True)
         else:
             self.rnn = nn.GRU(hparams.input_size, hparams.hidden_size,
                               num_layers=hparams.num_layers, batch_first=True)
         self.linear = nn.Linear(hparams.hidden_size, hparams.output_size)
         self.train_datapath = hparams.train_datapath
-        self.val_datapath = hparams.val_datapath
+        self.main_val_datapath = hparams.main_val_datapath
+        self.val_2_datapath = hparams.val_2_datapath
+        self.val_3_datapath = hparams.val_3_datapath
         self.test_datapath = hparams.test_datapath
         self.lr = hparams.lr
         self.batch_size = hparams.batch_size
@@ -42,7 +44,7 @@ class ProbabilityRNN(pl.LightningModule):
         self.logger.experiment.add_scalar('train_loss', loss, self.trainer.global_step)
         return {'loss': loss}
 
-    def validation_step(self, batch, batch_nb):
+    def validation_step(self, batch, batch_nb, dataloader_nb):
         x, y = batch
         logits = self(x)
         loss_fn = nn.BCEWithLogitsLoss()
@@ -50,9 +52,17 @@ class ProbabilityRNN(pl.LightningModule):
         return {'val_loss': loss}
 
     def validation_epoch_end(self, outputs):
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        self.logger.experiment.add_scalar('val_loss', avg_loss, self.trainer.global_step)
-        return {'avg_val_loss': avg_loss}
+        main_avg_loss = torch.stack([x['val_loss'] for x in outputs[0]]).mean()
+        second_avg_loss = torch.stack([x['val_loss'] for x in outputs[1]]).mean()
+        third_avg_loss = torch.stack([x['val_loss'] for x in outputs[2]]).mean()
+
+        self.logger.experiment.add_scalar('Validation/main_val_loss',
+                                          main_avg_loss, self.trainer.global_step)
+        self.logger.experiment.add_scalar('Validation/second_val_loss',
+                                          second_avg_loss, self.trainer.global_step)
+        self.logger.experiment.add_scalar('Validation/third_val_loss',
+                                          third_avg_loss, self.trainer.global_step)
+        return {'avg_val_loss': main_avg_loss}
 
     def test_step(self, batch, batch_nb):
         x, y = batch
@@ -77,9 +87,19 @@ class ProbabilityRNN(pl.LightningModule):
 
     def val_dataloader(self):
 
-        ising_dataset = ProbabilityDataset(filepath=self.val_datapath, data_key='ising_grids')
+        main_dataset = ProbabilityDataset(filepath=self.main_val_datapath, data_key='ising_grids')
+        main_dataloader = DataLoader(main_dataset, batch_size=self.batch_size,
+                                     num_workers=self.num_workers)
 
-        return DataLoader(ising_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+        second_dataset = ProbabilityDataset(filepath=self.val_2_datapath, data_key='ising_grids')
+        second_dataloader = DataLoader(second_dataset, batch_size=self.batch_size,
+                                       num_workers=self.num_workers)
+
+        third_dataset = ProbabilityDataset(filepath=self.val_3_datapath, data_key='ising_grids')
+        third_dataloader = DataLoader(third_dataset, batch_size=self.batch_size,
+                                      num_workers=self.num_workers)
+
+        return [main_dataloader, second_dataloader, third_dataloader]
 
     def test_dataloader(self):
 
@@ -117,7 +137,7 @@ class ProbabilityAttentionRNN(ProbabilityRNN):
     def forward(self, x):
         x, _ = self.rnn(x)
         x = x.permute(1, 0, 2)
-        x, attn_weights = self.attention(x, x, x) 
+        x, attn_weights = self.attention(x, x, x)
         x = x.permute(1, 0, 2)
         x = self.linear(x)
 
@@ -131,8 +151,7 @@ class ProbabilityAttentionRNN(ProbabilityRNN):
         self.logger.experiment.add_scalar('train_loss', loss, self.trainer.global_step)
         return {'loss': loss}
 
-    def validation_step(self, batch, batch_nb):
-        # TODO: Implement metric for energy
+    def validation_step(self, batch, batch_nb, dataloader_nb):
         x, y = batch
         logits, _ = self(x)
         loss_fn = nn.BCEWithLogitsLoss()
@@ -152,4 +171,4 @@ class ProbabilityAttentionRNN(ProbabilityRNN):
         prob = self.calculate_probability(x, y_hat)
         H = (-1/torch.tensor(self.beta))*(torch.log(prob))
 
-        return H   
+        return H
