4633e5c27a6f802d9ca78a3e115aabede7466e7c
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2020-05-28 20:28:18 -0500

    include training scripts

---
+++ energy_trainer.py
@@ -0,0 +1,114 @@
+import os
+import pytorch_lightning as pl
+from models.energy_models import EnergyAttentionRNN
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+
+parser.add_argument(
+    '--num_workers',
+    default=8,
+)
+
+parser.add_argument(
+    '--train_datapath',
+    default='./data/train_data.hdf5'
+)
+
+parser.add_argument(
+    '--main_val_datapath',
+    default='./data/val_data_beta_dot25.hdf5'
+)
+
+parser.add_argument(
+   'val_2_datapath',
+   default='./data/val_data_beta_dot125.hdf5'
+)
+
+parser.add_argument(
+   '--val_3_datapath',
+   default='./data/val_data_beta_dot5.hdf5'
+)
+
+parser.add_argument(
+    'test_datapath',
+    default='./data/test_data.hdf5'
+)
+
+parser.add_argument(
+    '--lstm',
+    default=False
+)
+
+parser.add_argument(
+    '--bidirectional',
+    default=False
+)
+
+parser.add_argument(
+    '--n_heads',
+    default=1
+)
+
+parser.add_argument(
+    '--lr',
+    default=0.0001
+)
+
+parser.add_argument(
+    '--batch_size',
+    default=512
+)
+
+parser.add_argument(
+    '--input_size',
+    default=1
+)
+
+parser.add_argument(
+    '--output_size',
+    default=1
+)
+
+parser.add_argument(
+    '--num_layers',
+    default=1
+)
+
+parser.add_argument(
+    '--hidden_size',
+    default=512
+)
+
+
+hparams = parser.parse_args()
+
+checkpoint_callback = pl.callbacks.ModelCheckpoint(
+          filepath=os.path.join(
+              os.getcwd(),
+              'checkpoints/energy/rnn_attn/GRU_1L_1H/{epoch}-{avg_val_loss:.3f}'),
+          monitor='avg_val_loss',
+          mode='min',
+          save_top_k=1,
+          period=1
+      )
+
+early_stopping = pl.callbacks.EarlyStopping(
+    monitor='avg_val_loss',
+    patience=5
+)
+
+logger = pl.loggers.TensorBoardLogger("energy_logs", name="rnn_attn", version="GRU_1L_1H")
+
+trainer = pl.Trainer(
+    gpus=1,
+    logger=logger,
+    checkpoint_callback=checkpoint_callback,
+    check_val_every_n_epoch=0.5,
+    max_epochs=20,
+    val_percent_check=0.01
+)
+
+model = EnergyAttentionRNN(hparams)
+
+trainer.fit(model)
